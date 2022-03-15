4b70357efd2bb1c16bc931e12425ef342821efbb
Exequiel Schiavo <exelu1495@gmail.com>
2022-03-14 21:07:23 -0300

    first commit

---
+++ js/datos.js
@@ -0,0 +1,129 @@
+const años = [
+  {
+    año: 2000,
+    valor: 1250,
+  },
+  {
+    año: 2001,
+    valor: 1255,
+  },
+  {
+    año: 2002,
+    valor: 1258,
+  },
+  {
+    año: 2003,
+    valor: 1261,
+  },
+  {
+    año: 2004,
+    valor: 1265,
+  },
+  {
+    año: 2005,
+    valor: 1270,
+  },
+  {
+    año: 2006,
+    valor: 1278,
+  },
+  {
+    año: 2007,
+    valor: 1283,
+  },
+  {
+    año: 2008,
+    valor: 1290,
+  },
+  {
+    año: 2009,
+    valor: 1310,
+  },
+  {
+    año: 2010,
+    valor: 1325,
+  },
+  {
+    año: 2011,
+    valor: 1335,
+  },
+  {
+    año: 2012,
+    valor: 1342,
+  },
+  {
+    año: 2013,
+    valor: 1348,
+  },
+  {
+    año: 2014,
+    valor: 1355,
+  },
+  {
+    año: 2015,
+    valor: 1362,
+  },
+  {
+    año: 2016,
+    valor: 1370,
+  },
+  {
+    año: 2017,
+    valor: 1375,
+  },
+  {
+    año: 2018,
+    valor: 1380,
+  },
+  {
+    año: 2019,
+    valor: 1390,
+  },
+  {
+    año: 2020,
+    valor: 1456,
+  },
+  {
+    año: 2021,
+    valor: 1465,
+  },
+  {
+    año: 2022,
+    valor: 1500,
+  },
+];
+
+const marcas = [
+  {
+    marca: "Peugeot",
+    valor: 4500,
+  },
+  {
+    marca: "Nissan",
+    valor: 6200,
+  },
+  {
+    marca: "Ford",
+    valor: 7800,
+  },
+  {
+    marca: "Chevrolet",
+    valor: 4200,
+  },
+  {
+    marca: "Jeep",
+    valor: 8500,
+  },
+  {
+    marca: "VW",
+    valor: 3400,
+  },
+  {
+    marca: "BMW",
+    valor: 6000,
+  },
+  {
+    marca: "Mercedes Benz",
+    valor: 10600,
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,97 @@
+const setMarcas = document.getElementById("marca");
+const setAño = document.getElementById("año");
+const getCobertura = document.getElementById("cobertura");
+const boton = document.getElementById("boton");
+const form = document.getElementById("formulario");
+const resultado = document.getElementById("resultados");
+
+/* defino las marcas de forma dinamica */
+
+marcas.forEach((datos) => {
+  const { marca, valor } = datos;
+
+  let opcion = document.createElement("option");
+
+  opcion.innerHTML += ` <option  name = "${marca}">${marca}</option>`;
+  opcion.setAttribute("value", `${valor}`);
+
+  setMarcas.appendChild(opcion);
+});
+
+/* defino los años  */
+
+años.forEach((años) => {
+  const { año, valor } = años;
+  let opcion = document.createElement("option");
+
+  opcion.innerHTML += ` <option>${año}</option>`;
+  opcion.setAttribute("value", `${valor}`);
+  opcion.setAttribute("name", `${año}`);
+
+  setAño.appendChild(opcion);
+});
+
+function Cotizar() {
+  let total;
+
+  resultado.innerHTML = ` <p class="animate__animated animate__bounce animate__infinite infinite">
+  Cargando...
+</p>`;
+  setTimeout(() => {
+    if (getCobertura.value === "completa") {
+      total = parseInt(setMarcas.value) + parseInt(setAño.value) + 5000;
+
+      resultado.innerHTML = `<p>Vehiculo : ${showChange()}</> <p>Año : ${showChangeAño()}</p> <p>Cobertura : Completa</p><h3>Monto a pagar: ${Formato(
+        total,
+        "UYU"
+      )}</h3>
+      
+
+      
+        
+       </p>`;
+    } else {
+      total = parseInt(setMarcas.value) + parseInt(setAño.value) + 1000;
+      resultado.innerHTML = `<p>Vehiculo : ${showChange()}</p><p>Año : ${showChangeAño()}</p> <p>Cobertura : Básica</p><h3>Monto a pagar: ${Formato(
+        total,
+        "UYU"
+      )}</h3> `;
+    }
+  }, 2000);
+}
+
+function showChange() {
+  let selected = setMarcas.options[setMarcas.selectedIndex].text;
+
+  return selected;
+}
+
+function showChangeAño() {
+  let selected = setAño.options[setAño.selectedIndex].text;
+  return selected;
+}
+
+function Validar() {
+  if (
+    setMarcas.value === "0" ||
+    setAño.value == "0 " ||
+    getCobertura.value == "0"
+  ) {
+    resultado.innerHTML =
+      '<p class = "alerta"> Debe seleccionar todos los campos </p>';
+
+    setTimeout(() => {
+      resultado.innerHTML = "";
+    }, 1500);
+  } else {
+    Cotizar();
+  }
+}
+
+const Formato = (valor, moneda) => {
+  return valor.toLocaleString("en-US", {
+    style: "currency",
+    currency: moneda,
+    minimumFractionDigits: 2,
+  });
+};
